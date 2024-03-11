e6da10d31e3cfde918b077b34a29914304176262
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-11 22:58:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Question no 29   More Conditional Tests:
+let favorite_fruits = ['orange', 'banana', 'watermelon'];
+if (favorite_fruits.includes('orange')) {
+    console.log(`oranges is my most favorite fruite.`);
+}
+if (favorite_fruits.includes('banana')) {
+    console.log(`banana is also my favorite fruite.`);
+}
+if (favorite_fruits.includes('watermelon')) {
+    console.log(`watermelon is fruite for me.`);
+}
---
+++ main.ts
@@ -0,0 +1,13 @@
+// Question no 29   More Conditional Tests:
+
+let favorite_fruits: string[] = ['orange','banana','watermelon'];
+
+if(favorite_fruits.includes('orange')){
+    console.log(`oranges is my most favorite fruite.`)
+}
+if(favorite_fruits.includes('banana')){
+    console.log(`banana is also my favorite fruite.`)
+}
+if(favorite_fruits.includes('watermelon')){
+    console.log(`watermelon is fruite for me.`)
+}
